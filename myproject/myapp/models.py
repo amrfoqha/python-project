ede03400ce7d00aa74b1c6d541ce9649ab428c7b
Ahmad <Ahmad.Badwan@axsos.academy.com>
2025-10-21 18:21:44 +0300

    msg

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name=models.CharField(max_length=45)
+    last_name=models.CharField(max_length=45)
+    email=models.EmailField(max_length=55)
+    phone=models.CharField(max_length=15)
+    location=models.CharField(max_length=100)
+    password=models.CharField(max_length=45)
+    level=models.IntegerField(max_length=5)
+    avatar=models.URLField(max_length=400,default="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")
+
+class Result(models.Model):
+    description=models.TextField()
+    recomindation=models.CharField(max_length=100)
+    user=models.ForeignKey(User,related_name='results',on_delete=models.CASCADE)
+    result=models.CharField(max_length=100)
