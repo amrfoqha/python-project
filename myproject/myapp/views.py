1dc693d2758f6f71644dbfae3f51cbe4b56ab6aa
Amr Foqha <amr.mofeed.foqha9@gmail.com>
2025-10-22 15:24:13 +0300

    test

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,7 +3,7 @@ from .models import *
 # Create your views here.
 def root(request):
 
-    return render(request,'registration.html')
+    return render(request,'main.html')
 
 def registration(request):
     return render(request,'registration.html')
